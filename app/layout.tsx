a0af7fb45111c1533af9adf95acb39d1c4312cbd
winnercreative <winnercreative0@gmail.com>
2026-05-13 12:57:06 +0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,20 +25,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <Script id="facebook-pixel" strategy="afterInteractive">
-        {`
-          !function(f,b,e,v,n,t,s)
-          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-          n.queue=[];t=b.createElement(e);t.async=!0;
-          t.src=v;s=b.getElementsByTagName(e)[0];
-          s.parentNode.insertBefore(t,s)}(window, document,'script',
-          'https://connect.facebook.net/en_US/fbevents.js');
-          fbq('init', '2776678569351422');
-          fbq('track', 'PageView');
-        `}
-      </Script>
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         {children}
       </body>
